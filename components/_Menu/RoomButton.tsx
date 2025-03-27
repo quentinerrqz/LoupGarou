6c8e7f0a24628e2107e262b90b70786d01d3e55f
Quentin Erriquez <quentinx07@gmail.com>
2025-03-27 18:34:50 +0100

    test again

--- components/_Menu/RoomButton.tsx
+++ components/_Menu/RoomButton.tsx
@@ -1,16 +1,17 @@
 "use client";
 import React, { FC } from "react";
 
-// type RoomButtonProps = {
-//   create: () => void;
-// };
+type RoomButtonProps = {
+  create: () => void;
+};
 
-const RoomButton = () => {
+const RoomButton = ({create}:RoomButtonProps) => {
   return (
     <div className="flex items-center justify-center h-full w-full backdrop-blur-sm bg-slate-900/30">
       <button
         type="button"
-        onClick={() => console.log("hello")}
+        onClick={() => alert("Room Created!")}
+        
         className="font-bold text-2xl bg-red-600 p-2 rounded-lg hover:bg-red-700 transition-all hover:scale-105 hover:text-white"
       >
         Create a Room

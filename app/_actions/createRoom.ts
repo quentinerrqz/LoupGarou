7bb6b998970d9403574c3989a69f15ab89da5257
Quentin Erriquez <quentinx07@gmail.com>
2025-03-27 19:12:04 +0100

    test fetching

--- app/_actions/createRoom.ts
+++ app/_actions/createRoom.ts
@@ -8,21 +8,26 @@ export async function createRoom() {
   const randomId = () => Math.random().toString(36).substring(2, 10);
   const id = randomId();
 
-  const req = await fetch(`${PARTYKIT_URL}/party/${id}`, {
-    method: "POST",
-    body: JSON.stringify(initialgameState),
-    headers: {
-      "Content-Type": "application/json",
-    },
-  });
-  if (!req.ok) {
-    console.log(req.status);
-    if (req.status === 404) {
-      notFound();
-    } else {
-      return notFound();
-    }
+  try {
+    const req = await fetch(`${PARTYKIT_URL}/party/${id}`, {
+      method: "POST",
+      body: JSON.stringify(initialgameState),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  } catch (error) {
+    console.error("Error creating room:", error);
+    return notFound();
   }
+  // if (!req.ok) {
+  //   console.log(req.status);
+  //   if (req.status === 404) {
+  //     notFound();
+  //   } else {
+  //     return notFound();
+  //   }
+  // }
   // await fetch(`${PARTYKIT_URL}/party/${id}`, {
   //   method: "POST",
   //   body: JSON.stringify(initialgameState),

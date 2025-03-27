96e651c9e7d4fd4bba9416e664754281dcbb52bc
Quentin Erriquez <quentinx07@gmail.com>
2025-03-27 18:27:11 +0100

    test

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ import { createRoom } from "./_actions/createRoom";
 export default function Home() {
   return (
     <main className="flex flex-col items-center justify-center h-full w-full">
-      <RoomButton create={createRoom} />
+      <RoomButton create={()=> console.log("hello")} />
     </main>
   );
 }
